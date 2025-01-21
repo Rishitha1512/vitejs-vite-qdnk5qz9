6eff8716509e2deab06f7eae05188ed04c707889
Rishitha1512 <katragadda.rishitha@kalvium.community>
2025-01-21 12:23:37 +0530

    completed the assignment

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,23 @@
 // write the component code here
+function Usercard() {
+    const profilePhoto = 'https://img.freepik.com/premium-vector/woman-avatar-profile-round-icon_24640-14047.jpg?w=1060';
+    const name = 'Sara';
+    const email = 'sara@gmail.com';
+    const phoneNumber = '7869005645';
+    const address = '1234 Elm Street, Springfield, IL, 62701';
+  return (
+    <div className="user-card">
+      <div className="profile-photo">
+        <img src={profilePhoto} alt="Profile" />
+      </div>
+      <div className="user-details">
+        <h3>{name}</h3>
+        <p><i>Email:</i> {email}</p>
+        <p><i>Phone:</i> {phoneNumber}</p>
+        <p><i>Address:</i> {address}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Usercard;
